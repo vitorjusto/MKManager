f968d93e9a9370dcc5f2eaaecc51759e876c2889
Pedro Masson <pedro.smasson@gmail.com>
2022-06-06 15:31:11 -0300

    new: cria value object celular

---
+++ MKManager/ValueObjects/Celular.cs
@@ -0,0 +1,27 @@
+﻿namespace MKManager.ValueObjects
+{
+    public struct Celular
+    {
+        private readonly string _numero;
+
+        public Celular(string numero) => _numero = numero;
+
+        public string ComDDI()
+        {
+            if (_numero.Contains("+55")) return _numero;
+
+            return $"+55 {_numero}";
+        }
+
+        public string SemDDI()
+        {
+            var verificarSeContemDDI = _numero.Contains("+55");
+
+            if (verificarSeContemDDI) return _numero.Substring(+3);
+
+            return _numero;
+        }
+
+        public static implicit operator Celular(string numero) => new Celular(numero);
+    }
+}
