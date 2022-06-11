275702e059fcbbd930396a78e55183873064b4b7
Pedro Masson <pedro.smasson@gmail.com>
2022-06-11 12:33:11 -0300

    new: adiciona mensagem de ola baseada na hora do dia

---
+++ MKManager/Helpers/Datas.cs
@@ -0,0 +1,19 @@
+﻿using Guna.UI2.WinForms;
+
+namespace MKManager.Helpers
+{
+    public class Datas
+    {
+        public static void VerificarHoraDoDia(Guna2HtmlLabel label) 
+        {
+            if (DateTime.Now.Hour >= 6 && DateTime.Now.Hour < 12)
+                label.Text = "Bom dia, Marina Silva!";
+
+            if (DateTime.Now.Hour >= 12 && DateTime.Now.Hour < 18)
+                label.Text = "Boa tarde, Marina Silva!";
+
+            if (DateTime.Now.Hour >= 18 && DateTime.Now.Hour < 6)
+                label.Text = "Boa noite, Marina Silva!";
+        }
+    }
+}
