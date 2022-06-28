56b0b8c3ca427e3545010aa4c7b826c042b73714
Pedro Masson <pedro.masson@inovafarma.com.br>
2022-06-28 16:58:59 -0300

    new: cria value object dinheiro

---
+++ MKManager/ValueObjects/Dinheiro.cs
@@ -0,0 +1,16 @@
+﻿namespace MKManager.ValueObjects
+{
+    public struct Dinheiro
+    {
+        private readonly decimal _dinheiro;
+
+        public Dinheiro(decimal dinheiro) => _dinheiro = dinheiro;
+
+        public override string ToString()
+        {
+            return _dinheiro.ToString("c");
+        }
+
+        public static implicit operator Dinheiro(decimal dinheiro) => new Dinheiro(dinheiro);
+    }
+}
