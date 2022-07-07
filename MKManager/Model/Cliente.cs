0ca29813d1bec83aabca478dcac8f5fcdbcd428f
Pedro Masson <pedro.masson@inovafarma.com.br>
2022-07-07 17:45:27 -0300

    new: adiciona models cliente e produto

---
+++ MKManager/Model/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace MKManager.Model
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string? Celular { get; set; }
+        public string? Email { get; set; }
+    }
+}
---
+++ MKManager/Model/Produto.cs
@@ -0,0 +1,13 @@
+﻿using MKManager.ValueObjects;
+
+namespace MKManager.Model
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Dinheiro PrecoCusto { get; set; }
+        public Dinheiro PrecoVenda { get; set; }
+        public int Estoque { get; set; }
+    }
+}
