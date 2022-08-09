eea07840afa3c7f1667ed748f645e43ff2c54f41
Pedro Masson <pedro.smasson@gmail.com>
2022-08-09 19:16:43 -0300

    new: cria tela inicial de produtos

--- MKManager/View/Produtos.Designer.cs
+++ MKManager/View/Produtos.Designer.cs
@@ -28,28 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnExcluirProduto = new Guna.UI2.WinForms.Guna2Button();
+            this.btnExibirProduto = new Guna.UI2.WinForms.Guna2Button();
+            this.btnCadastrarProduto = new Guna.UI2.WinForms.Guna2Button();
+            this.lblListagemProdutos = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.dgvProdutos = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PrecoCusto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PrecoVenda = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Estoque = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).BeginInit();
             this.SuspendLayout();
             // 
-            // guna2HtmlLabel1
+            // btnExcluirProduto
             // 
-            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel1.Location = new System.Drawing.Point(221, 187);
-            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
-            this.guna2HtmlLabel1.Size = new System.Drawing.Size(63, 17);
-            this.guna2HtmlLabel1.TabIndex = 0;
-            this.guna2HtmlLabel1.Text = "PRODUTOS";
+            this.btnExcluirProduto.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnExcluirProduto.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnExcluirProduto.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnExcluirProduto.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnExcluirProduto.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(0)))), ((int)(((byte)(1)))));
+            this.btnExcluirProduto.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnExcluirProduto.ForeColor = System.Drawing.Color.White;
+            this.btnExcluirProduto.Location = new System.Drawing.Point(12, 489);
+            this.btnExcluirProduto.Name = "btnExcluirProduto";
+            this.btnExcluirProduto.Size = new System.Drawing.Size(157, 55);
+            this.btnExcluirProduto.TabIndex = 9;
+            this.btnExcluirProduto.Text = "EXCLUIR PRODUTO";
+            // 
+            // btnExibirProduto
+            // 
+            this.btnExibirProduto.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnExibirProduto.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnExibirProduto.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnExibirProduto.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnExibirProduto.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnExibirProduto.ForeColor = System.Drawing.Color.White;
+            this.btnExibirProduto.Location = new System.Drawing.Point(215, 489);
+            this.btnExibirProduto.Name = "btnExibirProduto";
+            this.btnExibirProduto.Size = new System.Drawing.Size(157, 55);
+            this.btnExibirProduto.TabIndex = 8;
+            this.btnExibirProduto.Text = "EXIBIR PRODUTO";
+            // 
+            // btnCadastrarProduto
+            // 
+            this.btnCadastrarProduto.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnCadastrarProduto.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnCadastrarProduto.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnCadastrarProduto.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnCadastrarProduto.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(184)))), ((int)(((byte)(92)))));
+            this.btnCadastrarProduto.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCadastrarProduto.ForeColor = System.Drawing.Color.White;
+            this.btnCadastrarProduto.Location = new System.Drawing.Point(415, 489);
+            this.btnCadastrarProduto.Name = "btnCadastrarProduto";
+            this.btnCadastrarProduto.Size = new System.Drawing.Size(157, 55);
+            this.btnCadastrarProduto.TabIndex = 7;
+            this.btnCadastrarProduto.Text = "CADASTRAR PRODUTO";
+            // 
+            // lblListagemProdutos
+            // 
+            this.lblListagemProdutos.BackColor = System.Drawing.Color.Transparent;
+            this.lblListagemProdutos.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblListagemProdutos.Location = new System.Drawing.Point(188, 17);
+            this.lblListagemProdutos.Name = "lblListagemProdutos";
+            this.lblListagemProdutos.Size = new System.Drawing.Size(185, 27);
+            this.lblListagemProdutos.TabIndex = 6;
+            this.lblListagemProdutos.Text = "Listagem de Produtos";
+            // 
+            // dgvProdutos
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.dgvProdutos.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvProdutos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvProdutos.BackgroundColor = System.Drawing.Color.White;
+            this.dgvProdutos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvProdutos.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dgvProdutos.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvProdutos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvProdutos.ColumnHeadersHeight = 21;
+            this.dgvProdutos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Nome,
+            this.PrecoCusto,
+            this.PrecoVenda,
+            this.Estoque});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvProdutos.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvProdutos.EnableHeadersVisualStyles = false;
+            this.dgvProdutos.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dgvProdutos.Location = new System.Drawing.Point(12, 50);
+            this.dgvProdutos.Name = "dgvProdutos";
+            this.dgvProdutos.RowHeadersVisible = false;
+            this.dgvProdutos.RowTemplate.Height = 25;
+            this.dgvProdutos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProdutos.Size = new System.Drawing.Size(560, 419);
+            this.dgvProdutos.TabIndex = 5;
+            this.dgvProdutos.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.dgvProdutos.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.dgvProdutos.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.dgvProdutos.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.dgvProdutos.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.dgvProdutos.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.dgvProdutos.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dgvProdutos.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.dgvProdutos.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgvProdutos.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Segoe UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dgvProdutos.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.dgvProdutos.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.dgvProdutos.ThemeStyle.HeaderStyle.Height = 21;
+            this.dgvProdutos.ThemeStyle.ReadOnly = false;
+            this.dgvProdutos.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.dgvProdutos.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dgvProdutos.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Segoe UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dgvProdutos.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.dgvProdutos.ThemeStyle.RowsStyle.Height = 25;
+            this.dgvProdutos.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dgvProdutos.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.Visible = false;
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            // 
+            // PrecoCusto
+            // 
+            this.PrecoCusto.HeaderText = "Preço de Custo";
+            this.PrecoCusto.Name = "PrecoCusto";
+            // 
+            // PrecoVenda
+            // 
+            this.PrecoVenda.HeaderText = "Preço de Venda";
+            this.PrecoVenda.Name = "PrecoVenda";
+            // 
+            // Estoque
+            // 
+            this.Estoque.HeaderText = "Estoque";
+            this.Estoque.Name = "Estoque";
             // 
             // Produtos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 561);
-            this.Controls.Add(this.guna2HtmlLabel1);
+            this.Controls.Add(this.btnExcluirProduto);
+            this.Controls.Add(this.btnExibirProduto);
+            this.Controls.Add(this.btnCadastrarProduto);
+            this.Controls.Add(this.lblListagemProdutos);
+            this.Controls.Add(this.dgvProdutos);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Produtos";
             this.Text = "Produtos";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +209,15 @@
 
         #endregion
 
-        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2Button btnExcluirProduto;
+        private Guna.UI2.WinForms.Guna2Button btnExibirProduto;
+        private Guna.UI2.WinForms.Guna2Button btnCadastrarProduto;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblListagemProdutos;
+        private Guna.UI2.WinForms.Guna2DataGridView dgvProdutos;
+        private DataGridViewTextBoxColumn Id;
+        private DataGridViewTextBoxColumn Nome;
+        private DataGridViewTextBoxColumn PrecoCusto;
+        private DataGridViewTextBoxColumn PrecoVenda;
+        private DataGridViewTextBoxColumn Estoque;
     }
 }
