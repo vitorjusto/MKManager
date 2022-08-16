1a5116225fe8a54305ff8727498b11eeecfa3a7a
Pedro Masson <88439844+pedro-smasson@users.noreply.github.com>
2022-08-16 08:47:08 -0300

    new: adiciona métodos no AppService de clientes

--- MKManager/AppService/ClienteAppService.cs
+++ MKManager/AppService/ClienteAppService.cs
@@ -6,5 +6,8 @@ namespace MKManager.AppService
     public class ClienteAppService
     {
         public void CadastrarCliente(ClienteModel cliente) => ClienteRepository.CadastrarCliente(cliente);
+        public void ListarClientes(ClienteModel clientes) => ClienteRepository.ListarClientes(clientes);
+        public void AtualizarCliente(ClienteModel cliente) => ClienteRepository.AtualizarCliente(cliente);
+        public void ExcluirCliente(ClienteModel cliente) => ClienteRepository.ExcluirCliente(cliente);
     }
 }
