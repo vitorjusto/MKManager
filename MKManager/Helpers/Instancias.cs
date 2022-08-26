c4f08ae0e0f8a8c41283082676c8119de4e9338a
Pedro Masson <pedro.masson@inovafarma.com.br>
2022-08-26 14:28:33 -0300

    refactor: altera a forma em que se instanciam as telas

--- MKManager/Helpers/Instancias.cs
+++ MKManager/Helpers/Instancias.cs
@@ -4,10 +4,50 @@ namespace MKManager.Helpers
 {
     internal class Instancias
     {
-        public static TelaPrincipal telaPrincipal = new();
-        public static Clientes telaCliente = new();
-        public static Produtos telaProduto = new();
-        public static Relatorios telaRelatorio = new();
-        public static Ajuda telaAjuda = new();
+        public static TelaPrincipal telaPrincipal;
+        public static Clientes telaCliente;
+        public static Produtos telaProduto;
+        public static Relatorios telaRelatorio;
+        public static Ajuda telaAjuda;
+
+        public static TelaPrincipal ObterTelaPrincipal() 
+        {
+            if (telaPrincipal is null)
+                telaPrincipal = new TelaPrincipal();
+            
+            return telaPrincipal;
+        }
+
+        public static Clientes ObterTelaCliente()
+        {
+            if (telaCliente is null)
+                telaCliente = new Clientes();
+
+            return telaCliente;
+        }
+
+        public static Produtos ObterTelaProduto() 
+        {
+            if(telaProduto is null)
+                telaProduto = new Produtos();
+
+            return telaProduto;
+        }
+
+        public static Relatorios ObterTelaRelatorio()
+        {
+            if (telaRelatorio is null)
+                telaRelatorio = new Relatorios();
+
+            return telaRelatorio;
+        }
+
+        public static Ajuda ObterTelaAjuda()
+        {
+            if (telaAjuda is null)
+                telaAjuda = new Ajuda();
+
+            return telaAjuda;
+        }
     }
 }
--- MKManager/View/TelaMae.cs
+++ MKManager/View/TelaMae.cs
@@ -6,7 +6,7 @@ namespace MKManager.View
     {
         public TelaMae() => InitializeComponent();
 
-        private void TelaMae_Load(object sender, EventArgs e) => AdicionarFormAoPanel(Instancias.telaPrincipal);
+        private void TelaMae_Load(object sender, EventArgs e) => AdicionarFormAoPanel(Instancias.ObterTelaPrincipal());
 
         private void LimparBotoesAtivos()
         {
@@ -30,35 +30,35 @@ namespace MKManager.View
         {
             LimparBotoesAtivos();
             btnPaginaInicial.Checked = true;
-            AdicionarFormAoPanel(Instancias.telaPrincipal);
+            AdicionarFormAoPanel(Instancias.ObterTelaPrincipal());
         }
 
         private void btnClientes_Click(object sender, EventArgs e)
         {
             LimparBotoesAtivos();
             btnClientes.Checked = true;
-            AdicionarFormAoPanel(Instancias.telaCliente);
+            AdicionarFormAoPanel(Instancias.ObterTelaCliente());
         }
 
         private void btnProdutos_Click(object sender, EventArgs e)
         {
             LimparBotoesAtivos();
             btnProdutos.Checked = true;
-            AdicionarFormAoPanel(Instancias.telaProduto);
+            AdicionarFormAoPanel(Instancias.ObterTelaProduto());
         }
 
         private void btnRelatorios_Click(object sender, EventArgs e)
         {
             LimparBotoesAtivos();
             btnRelatorios.Checked = true;
-            AdicionarFormAoPanel(Instancias.telaRelatorio);
+            AdicionarFormAoPanel(Instancias.ObterTelaRelatorio());
         }
 
         private void btnAjuda_Click(object sender, EventArgs e)
         {
             LimparBotoesAtivos();
             btnAjuda.Checked = true;
-            AdicionarFormAoPanel(Instancias.telaAjuda);
+            AdicionarFormAoPanel(Instancias.ObterTelaAjuda());
         }
     }
 }
